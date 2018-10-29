aba7685451c2a3bca5094c66c3bf57166ab46ecb
Neha Sadhvi <iam@nehasadhvi.com>
2018-10-29 02:51:54 -0700

    Remove items from local storage

--- app.js
+++ app.js
@@ -7,9 +7,17 @@ const courseList = document.querySelector('#courses-list'),
 loadEventListeners();
 
 function loadEventListeners() {
+    //When new course is added
     courseList.addEventListener('click', buyCourse);
+
+    //When the remove button is clicked
     cartItem.addEventListener('click', removeCartItem);
+
+    //When Clear Cart button is clicked
     clearCart.addEventListener('click', clearShoppingCart);
+
+    //Document ready
+    document.addEventListener('DOMContentLoaded', loadFromLocalStorage);
 }
 
 //Functions
@@ -33,6 +41,26 @@ function getCartInfo(course) {
         id: course.querySelector('a').getAttribute('data-id')
     };
     addToCart(courseInfo);
+    addToLocalStorage(courseInfo);
+}
+
+// Add courses to local storage
+function addToLocalStorage (course) {
+    let courses = getFromLocalStorage();
+
+    courses.push(course);
+
+    localStorage.setItem('courses', JSON.stringify(courses))
+}
+
+//Get courses from local storage
+function getFromLocalStorage() {
+    if(localStorage.getItem('courses') === null) {
+        courses = [];
+    } else {
+        courses = JSON.parse(localStorage.getItem('courses'));
+    }
+    return courses;
 }
 
 //Add the course into the cart
@@ -53,19 +81,55 @@ function addToCart(course) {
             </td>
         </tr>
     `;
-
+    
     // Add row to shopping cart
     cartItem.appendChild(row);
 }
 
+//Remove single item from cart
 function removeCartItem(e) {
     if(e.target.classList.contains('remove')) {
         e.target.parentElement.parentElement.remove();
+        course = e.target.parentElement.parentElement;
+        courseId = course.querySelector('a').getAttribute('data-id');
     }
+    console.log(courseId)
+    //Remove from the local storage
+    removeItemLocalStorage(courseId);
 }
 
+//Remove single item from local storage
+function removeItemLocalStorage(id) {
+    let coursesLS = getFromLocalStorage();
+    
+    coursesLS.forEach(function(courseLS, index) {
+        if(courseLS.id === id) {
+            coursesLS.splice(index, 1);
+        }
+    });
+    
+    localStorage.setItem('courses', JSON.stringify(coursesLS));
+}
+
+//Clear all the items from shopping cart
 function clearShoppingCart(e) {
         while(cartItem.firstChild) {
             cartItem.firstChild.remove();
         }
+
+        removeAllFromLocalStorage();
+}
+
+//Remove all the course items from local storage
+function removeAllFromLocalStorage() {
+    localStorage.removeItem('courses');
+}
+
+//Get all the items stored in the local storage
+function loadFromLocalStorage() {
+    let coursesLS = getFromLocalStorage();
+    
+    coursesLS.forEach((courseLS) => {
+        addToCart(courseLS);
+    })
 }
